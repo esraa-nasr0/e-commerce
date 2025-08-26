7d3e07d5bbc181931daebad7c53bd59d3907361e
Esraa Nasr <esraaelbordeny@gmail.com>
2025-08-26 14:02:22 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // تأكد أن App.js موجود في نفس المجلد أو في المسار الصحيح
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import reportWebVitals from './reportWebVitals';
